a103fa377002dba229ea6de524e61e712d2d2d21
Daniel Fernandez <dan.fernandez1187@gmail.com>
2026-06-28 02:28:31 -0400

    Update 01_profile_variables.py

--- exercises/01_profile_variables.py
+++ exercises/01_profile_variables.py
@@ -4,4 +4,4 @@ goal = "learn Applied AI"
 hair = "dirty blonde (despite allegations that my hair is just brown)"
 ethnicity = "hispanic/latino"
 
-print (f"My name is {name}. I live in {Miami}. My goal is to {goal}. My hair is {hair}. I am also {ethnicity}.")
+print (f"My name is {name}. I live in {city}. My goal is to {goal}. My hair is {hair}. I am also {ethnicity}.")
