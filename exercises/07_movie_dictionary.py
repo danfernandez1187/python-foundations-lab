ea3a14975aa738856d5f032a3c2287c112a478a1
Daniel Fernandez <dan.fernandez1187@gmail.com>
2026-06-28 15:50:50 -0400

    Create 07_movie_dictionary.py

---
+++ exercises/07_movie_dictionary.py
@@ -0,0 +1,14 @@
+movie = []
+
+title = input("What is the movie title? ")
+year = input("What year did the movie come out? ")
+rating = input("What rating would you give it out of 10? ")
+
+movie["title"] = title
+movie["year"] = year
+movie["rating"] = rating
+
+print("Movie summary:")
+print(f"Title: {movie['title']}")
+print(f"Year: {movie['year']}")
+print(f"Rating: {movie['rating']}/10")
