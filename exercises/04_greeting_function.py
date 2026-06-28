832592957709cd6d742757c3e9ded2fbd08f3243
Daniel Fernandez <dan.fernandez1187@gmail.com>
2026-06-28 15:27:55 -0400

    Create 04_greeting_function.py

---
+++ exercises/04_greeting_function.py
@@ -0,0 +1,9 @@
+def create_greeting(name, city):
+  returnf"Hello, {name}! I heard you're building Python skills from {city}."
+
+user_name = input("What is your name? ")
+user_city = input("What city are you in? ")
+
+greeting = create_greeting(user_name, user_city)
+
+print(greeting)
