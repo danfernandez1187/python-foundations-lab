6957c3ffcdfdec7966d9848d5b0536a6ae0ff11f
Daniel Fernandez <dan.fernandez1187@gmail.com>
2026-06-28 15:44:58 -0400

    Create 06_grocery_list.py

---
+++ exercises/06_grocery_list.py
@@ -0,0 +1,14 @@
+groceries = []
+
+item_one = input("Enter your first grocery item: ")
+item_two = input("Enter uyour second grocery item: ")
+item_three = input("Enter your third grocery item: ")
+
+groceries.append(item_one)
+groceries.append(item_two)
+groceries.apend(item_three)
+
+print("Your grocery list:")
+
+for item in groceries:
+  print(f"- {item}")
